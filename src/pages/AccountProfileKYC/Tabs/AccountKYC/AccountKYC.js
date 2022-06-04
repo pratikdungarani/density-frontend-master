94ebb12e6b6966b049ad55915ab308687d6fb40f
pratik <me.pratikdungarani@gmail.com>
2022-06-04 10:09:34 +0530

    error solve islint

--- src/pages/AccountProfileKYC/Tabs/AccountKYC/AccountKYC.js
+++ src/pages/AccountProfileKYC/Tabs/AccountKYC/AccountKYC.js
@@ -7,50 +7,52 @@ import { Link } from "react-router-dom";
 const AccountKYC = () => {
   const [kycVerify, setKycVerify] = useState(false);
 
+  const AccountKYC = () => {
+    if (kycVerify) {
+      <Box>
+        <h1 className="accountkycTitle">KYC Details</h1>
+        <Box className="between-align" sx={{ mb: 3 }}>
+          <Box className="textPrimary">
+            <h6>KYC Status</h6>
+          </Box>
+          <Box className="verifiedLinks">
+            <p className="error pointer">Not verified</p>
+          </Box>
+        </Box>
+        <Box component="p" className="textCenter texGray authMessage">
+          Two Factor Authentication enabled. This helps keep your account safe. We strongly recommend you keep 2FA turned on.
+        </Box>
+        <Box>
+          <Link to="/accountProfileKYC" className="buttonCommon backround">
+            Verify KYC
+          </Link>
+        </Box>
+      </Box>;
+    } else {
+      <Box>
+        <h1 className="accountkycTitle">Personal Information</h1>
+        <Box className="between-align" sx={{ mb: 3 }}>
+          <Box className="textPrimary">
+            <h6>KYC Status</h6>
+          </Box>
+          <Box className="verifiedLinks">
+            <p className="success pointer" onClick={() => setKycVerify(!kycVerify)}>
+              Verifed
+            </p>
+          </Box>
+        </Box>
+        <CustomTextField name="firstname" label="First name" placeholder="John" disabled />
+        <CustomTextField name="lastname" label="Last name" placeholder="Doe" disabled />
+        <CustomTextField name="country" label="Country" placeholder="mail@abc.com" disabled />
+        <CustomTextField name="adharno" label="Aadhar Number" placeholder="XXXX XXXX XXXX XXXX" disabled />
+        <CustomTextField name="panNo" label="PAN Number" placeholder="ABCDE1234F" disabled />
+      </Box>;
+    }
+  };
+
   return (
     <Box className="accountkyc">
-      <Box className="accountkycInner">
-        {!kycVerify ? (
-          <Box>
-            <h1 className="accountkycTitle">Personal Information</h1>
-            <Box className="between-align" sx={{ mb: 3 }}>
-              <Box className="textPrimary">
-                <h6>KYC Status</h6>
-              </Box>
-              <Box className="verifiedLinks">
-                <p className="success pointer" onClick={() => setKycVerify(!kycVerify)}>
-                  Verifed
-                </p>
-              </Box>
-            </Box>
-            <CustomTextField name="firstname" label="First name" placeholder="John" disabled />
-            <CustomTextField name="lastname" label="Last name" placeholder="Doe" disabled />
-            <CustomTextField name="country" label="Country" placeholder="mail@abc.com" disabled />
-            <CustomTextField name="adharno" label="Aadhar Number" placeholder="XXXX XXXX XXXX XXXX" disabled />
-            <CustomTextField name="panNo" label="PAN Number" placeholder="ABCDE1234F" disabled />
-          </Box>
-        ) : (
-          <Box>
-            <h1 className="accountkycTitle">KYC Details</h1>
-            <Box className="between-align" sx={{ mb: 3 }}>
-              <Box className="textPrimary">
-                <h6>KYC Status</h6>
-              </Box>
-              <Box className="verifiedLinks">
-                <p className="error pointer">Not verified</p>
-              </Box>
-            </Box>
-            <Box component="p" className="textCenter texGray authMessage">
-              Two Factor Authentication enabled. This helps keep your account safe. We strongly recommend you keep 2FA turned on.
-            </Box>
-            <Box>
-              <Link to="/accountProfileKYC" className="buttonCommon backround">
-                Verify KYC
-              </Link>
-            </Box>
-          </Box>
-        )}
-      </Box>
+      <Box className="accountkycInner">{AccountKYC()}</Box>
     </Box>
   );
 };
--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -6,7 +6,8 @@ import Checkbox from "@mui/material/Checkbox";
 // MUI Icons
 import VisibilityOffOutlinedIcon from "@mui/icons-material/VisibilityOffOutlined";
 import VisibilityOutlinedIcon from "@mui/icons-material/VisibilityOutlined";
-
+// Formik
+import { Formik, Form } from "formik";
 import GoogleIcon from "assets/images/googleIcon.png";
 
 import { CustomTextField, CommonButton } from "components/ui/index";
@@ -16,60 +17,60 @@ import "./login.scss";
 
 const Login = () => {
   const [isVisible, setIsVisible] = React.useState(false);
+  // Initial Value
+  const initialValues = {
+    email: "",
+    password: ""
+  };
+
+  const onLogin = (value) => {};
   return (
     <>
       <Box className="loginWrap">
-        <h4 className="textPrimary textCenter welcomeTitle">
-          Welcome to Density.Exchange
-        </h4>
-        <Box className="loginWrapInner">
-          <h1 className="loginTitle">Login to your Account</h1>
-          <CustomTextField name="email" label="Email" placeholder="mail@abc.com" />
-          <CustomTextField
-            name="password"
-            type={isVisible ? "text" : "password"}
-            id="password"
-            label="Password"
-            InputProps={{
-              endAdornment: (
-                <InputAdornment position="end">
-                  <IconButton
-                    onClick={() => setIsVisible(!isVisible)}
-                    edge="end"
-                    className="visibilityIcon"
-                  >
-                    {isVisible
-                      ? <VisibilityOffOutlinedIcon />
-                      : <VisibilityOutlinedIcon />
-                    }
-                  </IconButton>
-                </InputAdornment>
-              )
-            }}
-          />
-          <div className="between-align">
-            <FormControlLabel
-              control={<Checkbox defaultChecked size="small" />}
-              label="Remember Me"
-              className="textPrimary rememberText"
-            />
-            <Link to="/forgot-password" className="primaryLink">
-              Forgot Password?
-            </Link>
-          </div>
-          <Box className="textPrimary textCenter" sx={{ my: 3 }}>
-            <p className="textGray">------------- or ------------- </p>
-          </Box>
-          <Box className="centre-align">
-            <button className="centre-align LoginBtn">
-              <img src={GoogleIcon} alt="GoogleIcon" />{" "}
-              <p className="textPrimary">Continue with Google</p>
-            </button>
-          </Box>
-          <Box sx={{ mt: 4 }}>
-            <CommonButton label="Login" className={"backround"} type="submit" />
-          </Box>
-        </Box>
+        <h4 className="textPrimary textCenter welcomeTitle">Welcome to Density.Exchange</h4>
+        <Formik initialValues={initialValues} onSubmit={(value) => onLogin(value)}>
+          {() => (
+            <Form id="signup-form">
+              <Box className="loginWrapInner">
+                <h1 className="loginTitle">Login to your Account</h1>
+                <CustomTextField name="email" label="Email" placeholder="mail@abc.com" />
+                <CustomTextField
+                  name="password"
+                  type={isVisible ? "text" : "password"}
+                  id="password"
+                  label="Password"
+                  InputProps={{
+                    endAdornment: (
+                      <InputAdornment position="end">
+                        <IconButton onClick={() => setIsVisible(!isVisible)} edge="end" className="visibilityIcon">
+                          {isVisible ? <VisibilityOffOutlinedIcon /> : <VisibilityOutlinedIcon />}
+                        </IconButton>
+                      </InputAdornment>
+                    )
+                  }}
+                />
+                <div className="between-align">
+                  <FormControlLabel control={<Checkbox defaultChecked size="small" />} label="Remember Me" className="textPrimary rememberText" />
+                  <Link to="/forgot-password" className="primaryLink">
+                    Forgot Password?
+                  </Link>
+                </div>
+                <Box className="textPrimary textCenter" sx={{ my: 3 }}>
+                  <p className="textGray">------------- or ------------- </p>
+                </Box>
+                <Box className="centre-align">
+                  <button className="centre-align LoginBtn">
+                    <img src={GoogleIcon} alt="GoogleIcon" /> <p className="textPrimary">Continue with Google</p>
+                  </button>
+                </Box>
+                <Box sx={{ mt: 4 }}>
+                  <CommonButton label="Login" className={"backround"} type="submit" />
+                </Box>
+              </Box>
+            </Form>
+          )}
+        </Formik>
+
         <Box sx={{ my: 3 }} className="textSecondary textCenter">
           Not Registered Yet?{" "}
           <Link to="/signup" className="primaryLink">
